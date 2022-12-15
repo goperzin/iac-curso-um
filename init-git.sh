fab77666c431ee4d8a0789ab8bea54d950ab4a9b
Leandro Lopes <goperzao@gmail.com>
2022-12-15 10:31:06 -0300

    add arquivos do terraform

---
+++ init-git.sh
@@ -0,0 +1,7 @@
+echo "# iac-curso-um" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/goperzin/iac-curso-um.git
+git push -u origin main
---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0574da719dca65348"
+  instance_type = "t2.micro"
+  key_name = "leandro-aws-key"
+  tags = {
+    Name = "wep-app"
+  }
+}
